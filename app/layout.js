552a9a1484f4f47b67bb7d96eac29f6d3eb5c907
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-05-19 01:25:26 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,10 @@
-"use client"; // บอก Next.js ว่าไฟล์นี้ควรจะทำงานบนไคลเอ็นต์เท่านั้น
-
-import React from 'react';
-import Script from 'next/script';
-
-const Layout = ({ children }) => {
-  // ตัวอย่างการใช้งาน Script สำหรับโหลด Facebook Pixel
+export default function RootLayout({ children }) {
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src="https://connect.facebook.net/en_US/fbevents.js"
-        onLoad={() => {
-          fbq('init', '819900423353826'); // ใช้งาน Pixel ID จริงของคุณ
-          fbq('track', 'PageView');
-        }}
-      />
-      <div>
-        <header>
-          <h1>My Website Header</h1>
-        </header>
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
         <main>{children}</main>
-        <footer>
-          <p>© 2024 My Website</p>
-        </footer>
-      </div>
-    </>
-  );
-};
-
-export default Layout;
+      </body>
+    </html>
+  )
+}
