b9f9541c141b2730c34a8b7173c14535f597d1a8
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-05-19 01:29:52 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './app.css'
+import './globals.css'
+
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
